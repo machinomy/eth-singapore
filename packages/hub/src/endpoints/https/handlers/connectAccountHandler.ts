ef49fdc7757f0375ac10a87da03fe7aa5e94e22c
Andrei Riaskov <ryaskov.work@gmail.com>
2018-12-09 09:26:55 +0800

    Add connectAccountHandler

--- packages/hub/src/endpoints/https/handlers/connectAccountHandler.ts
+++ packages/hub/src/endpoints/https/handlers/connectAccountHandler.ts
@@ -1,26 +1,28 @@
 import Logger from '@machinomy/logger'
 import * as Router from 'koa-router'
 import { DBManager } from '../../../DBManager'
+import { Record } from '../../../entity/Record'
 
 const log = new Logger('connectAccountHandler')
 
 export async function connectAccountHandler (ctx: Router.IRouterContext) {
   const body = ctx.request.body as any
+  const account = body['account']
+  const address = body['address']
+  const signature = body['signature']
 
-//   try {
-//     const connection = await DBManager.instance().dbConnection()
-//
-//     // const orders = await connection.getRepository(Record).createQueryBuilder('record')
-//     //   .where('record.account = :account', { account: account }).getMany()
-//     // if (orders.length !== 0) {
-//       // log.error(`Account ${account} has no connected eth address`)
-//       // ctx.status = 400
-//     // } else {
-//       // log.info(`Account ${account} has connected eth address`)
-//       ctx.status = 200
-//     }
-//   } catch  (e) {
-//     log.error(e)
-//     ctx.status = 400
-//   }
+  const record = new Record(account, address, signature)
+  try {
+    const connection = await DBManager.instance().dbConnection()
+
+    await connection.createQueryBuilder()
+      .insert()
+      .into(Record)
+      .values([ record.toJSON() ])
+      .execute()
+    ctx.status = 200
+  } catch  (e) {
+    log.error(e)
+    ctx.status = 400
+  }
 }
