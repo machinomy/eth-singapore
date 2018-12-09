730975b73a0c22381a00a1fed090113135578c5b
Andrei Riaskov <ryaskov.work@gmail.com>
2018-12-09 09:10:51 +0800

    Build now

--- packages/hub/src/endpoints/https/handlers/connectAccountHandler.ts
+++ packages/hub/src/endpoints/https/handlers/connectAccountHandler.ts
@@ -7,20 +7,20 @@ const log = new Logger('connectAccountHandler')
 export async function connectAccountHandler (ctx: Router.IRouterContext) {
   const body = ctx.request.body as any
 
-  try {
-    const connection = await DBManager.instance().dbConnection()
-
-    const orders = await connection.getRepository(Record).createQueryBuilder('record')
-      .where('record.account = :account', { account: account }).getMany()
-    if (orders.length !== 0) {
-      log.error(`Account ${account} has no connected eth address`)
-      ctx.status = 400
-    } else {
-      log.info(`Account ${account} has connected eth address`)
-      ctx.status = 200
-    }
-  } catch  (e) {
-    log.error(e)
-    ctx.status = 400
-  }
+//   try {
+//     const connection = await DBManager.instance().dbConnection()
+//
+//     // const orders = await connection.getRepository(Record).createQueryBuilder('record')
+//     //   .where('record.account = :account', { account: account }).getMany()
+//     // if (orders.length !== 0) {
+//       // log.error(`Account ${account} has no connected eth address`)
+//       // ctx.status = 400
+//     // } else {
+//       // log.info(`Account ${account} has connected eth address`)
+//       ctx.status = 200
+//     }
+//   } catch  (e) {
+//     log.error(e)
+//     ctx.status = 400
+//   }
 }
